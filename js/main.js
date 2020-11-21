1b0477f594e0661d57a5e7c4b626b03140b41326
aymen isfiaya <aymen.isfiaya@gmail.com>
2020-11-21 04:48:04 +0100

    add animation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var loader = document.getElementById('loader');
-
+var empty_heart = document.getElementById('empty_heart');
 setTimeout(function() {
     loader.style.display = 'none';
     document.body.style.overflow = 'auto';
 }, 3000);
+
+
